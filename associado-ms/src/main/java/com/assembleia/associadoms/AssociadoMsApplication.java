2cc37cc376dc973c83af1ebdc23fea76d1c4209f
Fabio Marsiaj <fabiomarsiaj@gmail.com>
2022-05-19 17:40:19 -0300

    bootstrap nos serviços

---
+++ associado-ms/src/main/java/com/assembleia/associadoms/AssociadoMsApplication.java
@@ -0,0 +1,13 @@
+package com.assembleia.associadoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssociadoMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssociadoMsApplication.class, args);
+	}
+
+}
---
+++ pauta-ms/src/main/java/com/assembleia/pautams/PautaMsApplication.java
@@ -0,0 +1,13 @@
+package com.assembleia.pautams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PautaMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PautaMsApplication.class, args);
+	}
+
+}
